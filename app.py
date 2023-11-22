384136ece9086e761eb360461b7a0cd7b248dd28
mrfakename <mrfakename@users.noreply.huggingface.co>
2023-11-22 16:48:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ A free demo of StyleTTS 2. **I am not affiliated with the StyleTTS 2 Authors.**
 
 **Before using this demo, you agree to inform the listeners that the speech samples are synthesized by the pre-trained models, unless you have the permission to use the voice you synthesize. That is, you agree to only use voices whose speakers grant the permission to have their voice cloned, either directly or by license before making synthesized voices public, or you have to publicly announce that these voices are synthesized if you do not have the permission to use these voices.**
 
-Is there a long queue on this space? Duplicate it and add a more powerful GPU to skip the wait!
+Is there a long queue on this space? Duplicate it and add a more powerful GPU to skip the wait! **Note: Thank you to Hugging Face for their generous GPU grant program!**
 
 **NOTE: StyleTTS 2 does better on longer texts.** For example, making it say "hi" will produce a lower-quality result than making it say a longer phrase.""")
     gr.DuplicateButton("Duplicate Space")
