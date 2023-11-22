297e7cea447a994d33a8517c2d3ef38e1bcab68a
mrfakename <mrfakename@users.noreply.huggingface.co>
2023-11-22 02:25:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ with gr.Blocks(title="StyleTTS 2", css="footer{display:none !important}", theme=
 
 [Paper](https://arxiv.org/abs/2306.07691) - [Samples](https://styletts2.github.io/) - [Code](https://github.com/yl4579/StyleTTS2)
 
-A free demo of StyleTTS 2. Not affiliated with the StyleTTS 2 Authors.
+A free demo of StyleTTS 2. **I am not affiliated with the StyleTTS 2 Authors.**
 
 **Before using this demo, you agree to inform the listeners that the speech samples are synthesized by the pre-trained models, unless you have the permission to use the voice you synthesize. That is, you agree to only use voices whose speakers grant the permission to have their voice cloned, either directly or by license before making synthesized voices public, or you have to publicly announce that these voices are synthesized if you do not have the permission to use these voices.**
 
