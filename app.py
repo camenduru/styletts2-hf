688e15e2dac2b09af80cf9dd381bd6c08afbe0c8
mrfakename <mrfakename@users.noreply.huggingface.co>
2023-11-23 00:53:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -110,6 +110,8 @@ with gr.Blocks(title="StyleTTS 2", css="footer{display:none !important}", theme=
 
 A free demo of StyleTTS 2. **I am not affiliated with the StyleTTS 2 Authors.**
 
+#### Help this space get to the top of HF's trending list! Please give this space a Like!
+
 **Before using this demo, you agree to inform the listeners that the speech samples are synthesized by the pre-trained models, unless you have the permission to use the voice you synthesize. That is, you agree to only use voices whose speakers grant the permission to have their voice cloned, either directly or by license before making synthesized voices public, or you have to publicly announce that these voices are synthesized if you do not have the permission to use these voices.**
 
 Is there a long queue on this space? Duplicate it and add a more powerful GPU to skip the wait! **Note: Thank you to Hugging Face for their generous GPU grant program!**
